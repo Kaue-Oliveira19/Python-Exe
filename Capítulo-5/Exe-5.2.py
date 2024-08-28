29d7e269f9c6ed6c5414ec0e127efe63cfd11b39
Kauê Oliveira <kaueoliveira3002@gmail.com>
2024-08-28 11:34:06 -0300

    Add files via upload

---
+++ Capítulo-5/Exe-5.2.py
@@ -0,0 +1,15 @@
+#######################################################################################################################################################################
+#Nome: Kauê Oliveira - Curso: Ciência da Computação - Email: kaueoliveira3002@gmail.com - Celular: 11 4100-2656
+#
+#Exercício 5.2 - Modifique o programa para exibir os números de 50 a 100.
+#######################################################################################################################################################################
+
+#Variável contador
+x = 50
+
+#Enquanto o valor da variável x for menor ou igual a 100, o laço continuará a fazendo tudo que tem dentro dele.
+while x <= 100:
+    #Será impresso o valor da variável x.
+    print(x)
+    #A variável x será incrementada, recebendo um novo valor.
+    x = x + 1
