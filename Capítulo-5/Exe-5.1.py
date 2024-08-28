16ded2e656eed4b46b94da409e5f3e378edbe149
Kauê Oliveira <kaueoliveira3002@gmail.com>
2024-08-28 11:28:38 -0300

    Add files via upload

---
+++ Capítulo-5/Exe-5.1.py
@@ -0,0 +1,15 @@
+#######################################################################################################################################################################
+#Nome: Kauê Oliveira - Curso: Ciência da Computação - Email: kaueoliveira3002@gmail.com - Celular: 11 94100-2656
+#
+#Exercício 5.1 - Modifique o programa para exibir os números de 1 a 100.
+#######################################################################################################################################################################
+
+#Variável contador
+x = 1
+
+#Enquanto o valor da variável x for menor ou igual a 100, o laço continuará realizando tudo que tem dentro dela. 
+while x <= 100:
+    #Irá ser impresso o valor da variável x.
+    print(x)
+    #x será incrementado, recebendo um novo valor.
+    x = x + 1
