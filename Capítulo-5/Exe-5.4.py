86e2e28d3d08fb05a5b5226b6ef80eb0e0a968a5
Kauê Oliveira <kaueoliveira3002@gmail.com>
2024-09-02 09:17:09 -0300

    Add files via upload

---
+++ Capítulo-5/Exe-5.4.py
@@ -0,0 +1,17 @@
+#######################################################################################################################################################################
+#Nome: Kauê Oliveira - Curso: Ciência da Computação - Email: kaueoliveira3002@gmail.com - Número: 11 94100-2656
+#
+#Exercício 5.4 - Modifique o programa anterior para imprimir de 1 até o número digitado pelo usuário, mas, dessa vez, apenas os números ímpares.
+#######################################################################################################################################################################
+
+#Variável contador.
+x= 1
+#O usuário irá digitar um número, que será armazenado na variável  num.
+num = float(input("Digite um número: "))
+
+#Enquanto x for menor ou igual a variável num, ele irá realizar tudo que estiver dentro do laço While.
+while x <= num:
+    #Será impresso na tela o valor da variável x.
+    print(x)
+    #A variável x será incrementada.
+    x = x + 1
