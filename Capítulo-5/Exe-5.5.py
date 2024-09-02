3f0021096cf1187351a94e9ecc8ee2b6762690fa
Kauê Oliveira <kaueoliveira3002@gmail.com>
2024-09-02 09:34:55 -0300

    Add files via upload

---
+++ Capítulo-5/Exe-5.5.py
@@ -0,0 +1,19 @@
+#######################################################################################################################################################################
+#Nome: Kauê Oliveira - Curso: Ciência da Computação - Email: kaueoliveira3002@gmail.com - Número: 11 94100-2656
+#
+#Exercício 5.5 - Reescreva o programa anterior para escrever os 10 primeiros múltiplos de 3.
+#######################################################################################################################################################################
+
+#Variável contador.
+x = 1
+#Variável
+y = 10
+
+#Enquanto a variável x for menor ou igual a y, ela realizará tudo que estiver dentro do laço While.
+while x <= y:
+    #A variável multiplo receberá a multiplicação do número 3 com a variável x.
+    multiplo = 3 * x
+    #Será impresso na tela o resultado da multiplicação 
+    print(multiplo)
+    #A variável x será incrementada.
+    x = x + 1
